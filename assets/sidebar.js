e152ef49095ddd9d572001cc1d81176b205d6274
gangdolph <defkloef@gmail.com>
2025-09-02 21:01:02 -0400

    feat: add language toggle flag

--- assets/sidebar.js
+++ assets/sidebar.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
   const toggle = document.querySelector('.side-nav-toggle');
   const sideNav = document.querySelector('.side-nav');
+  const body = document.body;
   if (toggle && sideNav) {
     toggle.addEventListener('click', () => {
       sideNav.classList.toggle('open');
+      body.classList.toggle('nav-open');
     });
   }
 });
